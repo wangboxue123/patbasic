6c9cc1d48f1356d555e44ff8720728fc128c6b93
wangboxue123 <33798496+wangboxue123@users.noreply.github.com>
2018-04-01 09:39:58 +0800

    Add files via upload

---
+++ 1043.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+# include<iostream>
+using namespace std;
+
+int main(){
+	int n,i,m;
+	cin >> n;
+	int a[n];
+	m=0; 
+	for(i = 0; i < n; i++){
+		cin >> a[i];
+		m+=a[i];
+	}
+	m*=(n-1)*11;
+	cout << m; 
+	return 0; 
+}
